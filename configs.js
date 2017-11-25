db07617ed87bdb1ad710466ef75632aa05d60c14
Mike <michael.westbrooks@redroostertec.com>
2017-11-25 08:42:42 -0500

    Second commit

---
+++ configs.js
@@ -0,0 +1,8 @@
+//  MARK:- This file will contain all of my keys and etc. for interacting with various services. Ensure that these keys also exist in the config settings on Heroku. process.env.Name-of-Config
+//
+
+module.exports = {
+    port: 1337,
+    url: 'mongodb://dadhive-rrtech-admin:Bigmike031991@ds119406.mlab.com:19406/heroku_b5vgf1fg',
+    secret: 'wjiofn349w84h93w8hg4nilrwubg4p983h402pqh'
+}
