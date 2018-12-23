b2f446c3b95fb09e26b723a9c2cbfd95b3023114
michael.westbrooks@delta.com <michael.westbrooks@delta.com>
2018-12-22 23:40:40 -0500

    Updating project.

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 'use strict';
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
+
 const express       = require('express');
+const bcrypt        = require('bcryptjs');
 const bodyParser    = require('body-parser');
+const path          = require('path');
 const configs       = require('./configs.js');
-const MongoClient   = require('mongodb').MongoClient;
-const Mongoose      = require('mongoose');
 const firebase      = require('firebase');
 const session       = require('client-sessions');
+const router        = express.Router();
+const nodemailer    = require('nodemailer');
+const randomstring  = require('randomstring');
+const xoauth2       = require('xoauth2');
+const NodeCache         = require('node-cache');
 
-//  MARK:- Handle everything else.
-var port = process.env.PORT || configs.port;
-var jwtsec = process.env.JWT_SECRET || configs.secret;
-var databaseUri = process.env.MONGODB_URI || configs.url;
+var oneDay          = 86400000;
+var basePath        = configs.base;
+var basePathRoutes  = configs.baseRoutes;
+var basePathViews   = configs.baseViews;
+var port            = process.env.PORT || configs.port;
+var jwtsec          = process.env.JWT_SECRET || configs.secret;
+var nodemailerUsr   = process.env.NODEMAIL_USR || configs.nodemailusr;
+var nodemailerPass  = process.env.NODEMAIL_PSW || configs.nodemailpass;
+var nodemailerClientID = process.env.NODEMAIL_CLIENT || configs.nodemailerclientid;
+var nodemailerClientSecret = process.env.NODEMAIL_CLIENTSEC || configs.nodemailerclientsecret;
+var nodemailerClientToken = process.env.NODEMAIL_REFTOKEN || configs.nodemailerclienttoken;
 
-//  MARK:- Set up routes.
-var AuthController = require('./routes/auth/AuthController');
-var MainController = require('./routes/main/index');
-var APIController = require('./routes/api');
-var HomeController = require('./routes/main/home');
-
-//  MARK:- Set up express app.
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-// Serve static assets from the /public folder
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public', {
+    maxage: oneDay * 21
+}));
 app.use(session({
     cookieName: process.env.COOKIENAME || configs.cookiename,
     secret: process.env.COOKIESEC || configs.cookiesecret,
@@ -34,7 +40,23 @@ app.use(session({
     activeDuration: 5 * 60 * 1000,
 }));
 
-//  MARK:- Setup Firebase App
+var nodeCache = new NodeCache({ 
+    stdTTL: 0, 
+    checkperiod: 604800,
+    useClones: true
+});
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: nodemailerUsr,
+        clientId: nodemailerClientID,
+        clientSecret: nodemailerClientSecret,
+        refreshToken: nodemailerClientToken
+    }
+});
+
 var config = {
     apiKey: process.env.FIRAPIKEY || configs.firapikey,
     authDomain: process.env.FIRDOM || configs.firauthdomain,
@@ -45,12 +67,6 @@ var config = {
 };
 firebase.initializeApp(config);
 
-//  MARK:- Use Routes
-app.use('/', MainController);
-app.use('/api/json/v1', APIController); // r
-app.use('/auth', AuthController);
-app.use('/home', HomeController);
-
 app.all('/assets/*', function(req, res) {
     res.sendStatus(404);
 });
@@ -58,8 +74,448 @@ app.all('/data/*', function(req, res) {
     res.sendStatus(404);
 });
 
-//  MARK:- See if mongoDB is running & start server
-const httpServer = require('http').createServer(app);
+
+router.get('/', function(req, res) {
+    if (isLive === true || isLive === "true") {
+        res.status('200').render('main');
+    } else {
+        res.redirect('/');
+    }
+});
+
+router.get('/', function(req, res) {
+    res.status('200').render('index');
+});
+
+router.get('/preregister', function(req, res){
+    res.status('200').render('preregister');
+});
+
+router.get('/register', function(req, res){
+    if (isLive === true || isLive === "true") {
+        res.status('200').render('register');
+    } else {
+        res.redirect('/');
+    }
+});
+
+//  API
+router.post('/mo-login', function(req, res) {
+    console.log(req.body);
+    if (!req.body.email) return res.status(401).send({
+        response: 401,
+        message: "Did not provide any information. Please try again.",
+        error: "Error"
+    });
+    //  MARK:- Step 1 & 2: Create Token using device ID and set an expiration date of 24 hours.
+    var token = jwt.sign({
+        id: req.body.email
+    }, configs.secret, {
+        expiresIn: 21600
+    });
+    
+    //  MARK:- Step 3: Send Response Back
+    res.status(200).send({
+        response: 200,
+        message: "Data was returned from the database.",
+        data: {
+            auth_token: token
+        }
+    });
+    
+});
+
+router.post('/mo-lookup', function(req, res) {
+    console.log(req.body);
+    var code = req.body.regcode;
+    PreRegUser.findOne({
+        uregkey: code
+    }, function(error, result){
+        if (error) return res.status(500).send({
+            response: 500,
+            message: "Failed to retrieve data.",
+            error: error
+        });
+
+        console.log(result);
+        res.status(200).send({
+            response: 200,
+            message: "SUCCESS!",
+            data: result,
+            error: null
+        });
+    });
+})
+
+router.post('/mo-register', function(req, res) {
+    var idForUseWithToken = req.body.id;
+    console.log(req.body);
+    if (!idForUseWithToken) return res.status(401).send({
+        response: 401,
+        message: "Did not provide any information. Please try again.",
+        error: "Error"
+    });
+    //  MARK:- Step 1 & 2: Create Token using device ID and set an expiration date of 24 hours.
+    var token = jwt.sign({
+        id: req.body.id
+    }, configs.secret, {
+        expiresIn: 21600
+    });
+    
+    console.log(req.body);
+    //  MARK:- Step 3: Send Response Back
+    res.status(200).send({
+        response: 200,
+        message: "Data was returned from the database.",
+        data: {
+            auth_token: token
+        }
+    });
+});
+
+router.post('/authtoken', function(req, res){
+    var idForUseWithToken = req.body.id;
+    console.log(req.body);
+    if (!idForUseWithToken) return res.status(401).send({
+        response: 401,
+        message: "Did not provide any information. Please try again.",
+        error: "Error"
+    });
+    //  MARK:- Step 1 & 2: Create Token using device ID and set an expiration date of 24 hours.
+    var token = jwt.sign({
+        id: req.body.id
+    }, configs.secret, {
+        expiresIn: 21600
+    });
+    
+    //  MARK:- Step 3: Send Response Back
+    res.status(200).send({
+        response: 200,
+        message: "Data was returned from the database.",
+        data: {
+            auth_token: token
+        }
+    });
+});
+
+router.get('/retrievekeys', function(req, res){
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({
+        response: 401,
+        message: "Token has not been provided. Please try again.",
+        error: "Error"
+    });
+    
+    jwt.verify(token, configs.secret, function(err, decoded){
+        if (err) return res.status(500).send({
+            response: 500,
+            message: "Failed to authenticate token. Please try again",
+            error: err
+        });
+        res.status(200).send({
+            response: 200,
+            message: "Data was returned from the database.",
+            data: {
+                api: process.env.APP_ID,
+                masterKey: process.env.SERVER_URL,
+                awsaccess: process.env.AWS_ACCESS_KEY_ID,
+                awssecret: process.env.AWS_SECRET_ACCESS_KEY,
+                s3bucket: process.env.S3_BUCKET_NAME
+            }
+        });
+    });
+});
+
+router.get('/sessioncheck', function(req, res){
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({
+        response: 401,
+        message: "Token has not been provided. Please try again.",
+        error: "Error"
+    });
+    
+    jwt.verify(token, configs.secret, function(err, decoded){
+        if (err) return res.status(500).send({
+            response: 500,
+            message: "Failed to authenticate token. Please try again",
+            error: err
+        });
+        res.status(200).send({
+            response: 200,
+            message: "Data was returned from the database.",
+            data: {
+                api: process.env.APP_ID,
+                masterKey: process.env.MASTER_KEY,
+                server: process.env.SERVER_URL
+            }
+        });
+    });
+});
+
+//  AUTHCONTROLLER
+router.post('/register', function(req, res) {
+    var userId = randomstring.generate(10)
+    firebase.auth().createUserWithEmailAndPassword(req.body.uemail, req.body.upswd).then(function() {
+        var userObj = {
+            uid: userId,
+            uname: req.body.uname,
+            uemail: req.body.uemail,
+            umar_status: req.body.umar_status,
+            uchildren: req.body.uchildren,
+            updatedAt: Date(),
+            createdAt: Date(),
+            uphotoUrl: null,
+            ulinkedin: null,
+            ufacebook: null,
+            uinstagram: null,
+            upostscount: 0,
+            ufollowcount: 0,
+            ufollwerscount: 0,
+            ufavoritescount: 0,
+            ulastlogin: Date()
+        } 
+        firebase.database().ref('users/'+firebase.auth().currentUser.uid).set(userObj).then(function(){
+            //  MARK:- Send Response
+            res.status(200).send({
+                response: 200,
+                message: "Data was returned from the database.",
+                data: {
+                    auth_token: "token"
+                }
+            });
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            
+            res.status(500).send({
+                response: 500,
+                message: errorMessage,
+                error: error
+            });
+        });
+        
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        res.status(500).send({
+            response: 500,
+            message: errorMessage,
+            error: error
+        });
+    });
+});
+
+//  PREREGISTER USER
+router.post('/preregister', function(req, res){
+    // MARK:- Step 1: Create user
+    var regKey; 
+    if (req.body.uname.split(" ").length > 1) {
+        regKey = req.body.uname.split(" ")[0][0] + req.body.uname.split(" ")[1][0] + randomstring.generate(6)
+    } else {
+        regKey = req.body.uname.split(" ")[0][0] + randomstring.generate(7)
+    }
+    
+    
+    PreRegUser.create({
+        uname: req.body.uname,
+        uemail: req.body.uemail,
+        umar_status: req.body.umar_status,
+        uchildren: req.body.uchildren,
+        updatedAt: Date(),
+        createdAt: Date(),
+        ulinkedin: req.body.ulinkedin,
+        ufacebook: req.body.ufacebook,
+        uinstagram: req.body.uinstagram,
+        uregkey: regKey.toUpperCase()
+    }, function(err, user){
+        if (err) return res.status(500).send({
+            response: 500,
+            message: "There was a problem registering the user.",
+            error: err
+        });
+        var mailOptions = {
+            from: nodemailerUsr,
+            to: req.body.uemail,
+            subject: 'DadHive Pre-Registration Success!',
+            html: '<b>Welcome to the DadHive, ' + req.body.uname + '!</b><br><br>My name is Mike, founder of the DadHive and Father\'s rights advocate. Did you know that one of the leading reasons as to why fathers lose custody battles is because of the <b>fear of the common belief that men ALWAYS lose</b>?<br><br>The fear generally stems from a lack of knowledge and understanding of the laws and protocol regarding obtaining your rights as a father.<br><br>I founded the DadHive to create a platform that will serve as a resource and outlet for you to hold yourself accountable as you travel the journey called <b>fatherhood</b>. We are almost finished with the platform, but it is our hope that our platform will be of a great resource for you and anyone you know that is a father.<br><br>Your pre-registration code is <b>' + regKey.toUpperCase() + '</b>. You can use this code to expidite login into the platform when it is launched.<br><br>Cheers!<br><br><b>Michael Westbrooks</b>, <i>Founder of the DadHive</i>'
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+                res.status(500).send({
+                    response: 500,
+                    message: "There was a problem registering the user.",
+                    error: err
+                });
+            } else {
+                console.log('Email sent: ' + info.response);
+                //  MARK:- Send Response
+                res.status(200).send({
+                    response: 200,
+                    message: "SUCCESS!",
+                    error: null
+                });
+            }
+        });
+    });
+});
+
+//  LOGIN a RETURNING USER
+router.post('/login', function(req, res){
+    if (isLive === true || isLive === "true") {
+        console.log(req.body);
+        firebase.auth().signInWithEmailAndPassword(req.body.uemail, req.body.upswd).then(function() {
+            firebase.auth().onAuthStateChanged(function(user) {
+                if (user) {   
+                    console.log(user);
+                    res.redirect('/home');
+                }
+            });
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            res.status(500).send({
+                response: 500,
+                message: errorMessage,
+                error: error
+            });
+        });
+    } else {
+        res.redirect('/');
+    }
+});
+
+router.post('/regcode', function(req, res){
+    console.log(req.body);
+    var code = req.body.regcode;
+    PreRegUser.findOne({
+        uregkey: code
+    }, function(error, result){
+        if (error) return res.status(500).send({
+            response: 500,
+            message: "Failed to retrieve data.",
+            error: error
+        });
+
+        console.log(result);
+        res.status(200).send({
+            response: 200,
+            message: "SUCCESS!",
+            data: result,
+            error: null
+        });
+    });
+});
+
+//  USERCONTROLLER
+//  FIND ME USERS in the DB
+router.get('/me', function(req, res){
+    //  MARK:- Ensure token is validated
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({
+        response: 401,
+        message: "Token has not been provided. Please try again.",
+        error: err
+    });
+    
+    jwt.verify(token, configs.secret, function(err, decoded){
+        if (err) return res.status(500).send({
+            response: 500,
+            message: "Failed to authenticate token. Please try again",
+            error: err
+        });
+        
+        /*User.find({}, function(err, users){
+            if (err) return res.status(500).send({
+                response: 500,
+                message: "There was a problem adding the information to the database.",
+                data: users,
+                error: err
+            });
+            if (users) {
+                res.status(200).send({
+                    response: 200,
+                    message: "Data was returned from the database.",
+                    data: users
+                });
+            } else {
+                res.status(404).send({
+                    response: 404,
+                    message: "There was no data returned from the database.",
+                    data: users
+                });
+            }
+        })*/
+    })
+})
+
+//  FIND ALL USERS in the DB
+router.get('/', function(req, res){
+    User.find({}, function(err, users){
+        if (err) return res.status(500).send({
+            response: 500,
+            message: "There was a problem adding the information to the database.",
+            data: users,
+            error: err
+        });
+        if (users) {
+            res.status(200).send({
+                response: 200,
+                message: "Data was returned from the database.",
+                data: users
+            });
+        } else {
+            res.status(404).send({
+                response: 404,
+                message: "There was no data returned from the database.",
+                data: users
+            });
+        }
+    })
+})
+
+//  MARK:- FIND a USER by QUERY in the DB
+router.post('/search', function(req, res){
+    var query = req.body;
+    console.log("Users get query is:");
+    console.log(query);
+    User.find(query, function(err, users){
+        if (err) return res.status(500).send({
+            response: 500,
+            message: "There was a problem adding the information to the database.",
+            data: users,
+            error: err
+        });
+        if (users) {
+            res.status(200).send({
+                response: 200,
+                message: "Data was returned from the database.",
+                data: users
+            });
+        } else {
+            res.status(404).send({
+                response: 404,
+                message: "There was no data returned from the database.",
+                data: users
+            });
+        }
+    })
+})
+
+var httpServer = require('http').createServer(app);
+httpServer.setTimeout(72000000);
+httpServer.timeout = 72000000;
+httpServer.agent= false;
+httpServer.listen(port, function() {
+    console.log('DadHive running on port ' + port + '.');
+});
+
 const io = require('socket.io')(httpServer);
 
 io.on('connection', function(socket){
@@ -69,13 +525,10 @@ io.on('connection', function(socket){
     });
 });
 
-Mongoose.connect(databaseUri, (error, db) => {
-    if (error) return console.log(error);  
-    httpServer.listen(port, function() {
-        console.log('DadHive running on port ' + port + '.');
-    });
-});
-
-
-// This will enable the Live Query real-time server
-//ParseServer.createLiveQueryServer(httpServer);
+module.exports.port = port;
+module.exports.configs = configs;
+module.exports.basePath = configs.base;
+module.exports.basePathRoutes = configs.baseRoutes;
+module.exports.basePathViews = configs.baseViews;
+module.exports.firebase = firebase;
+module.exports.cache = nodeCache;
--- configs.js
+++ configs.js
@@ -1,8 +1,13 @@
-//  MARK:- This file will contain all of my keys and etc. for interacting with various services. Ensure that these keys also exist in the config settings on Heroku. process.env.Name-of-Config
-//
+'use strict';
+
+const path          = require('path');
 
 module.exports = {
-    port: 1337,
+    port: 3000,
+    base: __dirname,
+    basePublic: path.join(__dirname, '/public/'),
+    baseRoutes: path.join(__dirname, '/routes'),
+    baseViews: path.join(__dirname, '/views/'),
     url: 'mongodb://dadhive-rrtech-admin:Bigmike031991@ds119406.mlab.com:19406/heroku_b5vgf1fg',
     secret: 'wjiofn349w84h93w8hg4nilrwubg4p983h402pqh',
     nodemailusr: "thedadhive@gmail.com",
@@ -18,5 +23,8 @@ module.exports = {
     firprojectid: "dadhive-c1b2f",
     firstoragebucket: "dadhive-c1b2f.appspot.com",
     firmessagingsenderid: "259732187078",
+    s3AccessKey: '',
+    s3SecretKey: '',
+    s3bucket: '',
     isLive: false
 }
