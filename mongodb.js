f4e42c87c0d54af11410adb27ac423d976d851fd
Michael Westbrooks <mwestbrooksjr@gmail.com>
2019-09-25 16:13:10 -0400

    DadHive-Test (#6)
    * Adding order to media array items.
    
    * Updated the compound query function
    
    * Adding snapshot key back to data model.
    
    * updating data models.
    
    * Updating model again.
    
    * Updating parameters call again.
    
    * Updating more stuff.
    
    * Fixing
    
    * Fixed error.
    
    * okokok
    
    * okokok
    
    * OKOK
    
    * Updating stuff.
    
    * Updating and adding mongoDB to the mix.
    
    * Updating api endpoints.
    
    * Updating data transformation for section 1.
    
    * Adding image icon to data transformation.
    
    * Added number of kids to to data model and transformation.
    
    * updating getUsersMongoDB
    
    * Fixing up stuff.
    
    * more changes
    
    * added support for up to 6 images.
    
    * Updated endpoint call.
    
    * Increasing size of query.
    
    * Updating user model.
    
    * Fixing saveLocation query.
    
    * Fixed geoquery.
    
    * Another update to geoquery.
    
    * Adding php api stuff.
    
    * Updated conversation API.
    
    * Updated find connversations api.
    
    * Updating create message api.
    
    * Updating updateConversation
    
    * Updating api.
    
    * Updating stuff.
    
    * Adding age range to query.
    
    * New thing.
    
    * OK
    
    * New api
    
    * Updating api.
    
    * update api.
    
    * updating again.
    
    * First commit to dev project.
    
    * Did more updates.
    
    * Updated backend logic.
    
    * Added some delting endpoints.
    
    * Updated findConversations endpoint.
    
    * Updated registration form.
    
    * Prior to updating all pods.
    
    * Added firebase functions to project.
    
    * Added mapping functionality.
    
    * Updating queries.
    
    * Updating signup function
    
    * Overhaul of api.
    
    * Added additional stuff to edit user.
    
    * Finishing up API
    
    * Making a bunch of updates.
    
    * Fixing merge conflict in package-lock.json
    
    * Making major updates.
    
    * Updating web app
    
    * More test stuff.
    
    * Removing unnecessary files.

---
+++ mongodb.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const MongoClient       = require('mongodb').MongoClient;
+const assert            = require('assert');
+const configs           = require('./configs');
+
+//  MARK:- Setup MongoDB App
+var moClient;
+var db;
+
+function setupMongoClient(callback) {
+    MongoClient.connect(configs.mongoUrl, { useNewUrlParser: true }, function(err, client) {
+        if (!err) {
+            moClient = client
+            callback(true)
+        } else {
+            console.log(err);
+            callback(false);
+        }
+    })
+}
+
+module.exports.setup = function setup() {
+    console.log('Setting up MongoDB');
+    setupMongoClient(function(success) {
+        if (success) {
+            console.log("Successfully set up mongo client");
+        } else {
+            console.log("Unsuccessfully set up mongo client");
+        }
+    });
+};
+
+module.exports.mongodb_client = function returnMongoDBMainObject(callback) {
+    callback(moClient);
+}
+
+module.exports.usergeo = function returnMongoUserGeoDB(callback) {
+    var db = moClient.db(configs.mongoid);
+    var collection = db.collection('user-geo');
+    callback(collection);
+}
+
+module.exports.actioncol = function returnMongoUserGeoDB(callback) {
+    var db = moClient.db(configs.mongoid);
+    var collection = db.collection('action');
+    callback(collection);
+}
+
+module.exports.mapitemcol = function returnMongoMapItemGeoDB(callback) {
+    var db = moClient.db(configs.mongoid);
+    var collection = db.collection('map-items');
+    callback(collection);
+}
+
+module.exports.convoscol = function returnMongoUserGeoDB(callback) {
+    var db = moClient.db(configs.mongoid);
+    var collection = db.collection('conversations');
+    callback(collection);
+}
+
+module.exports.postscol = function returnMongoUserGeoDB(callback) {
+    var db = moClient.db(configs.mongoid);
+    var collection = db.collection('posts');
+    callback(collection);
+}
+
+module.exports.categoriescol = function returnMongoUserGeoDB(callback) {
+    var db = moClient.db(configs.mongoid);
+    var collection = db.collection('categories');
+    callback(collection);
+}
+
+module.exports.engagementscol = function returnMongoUserGeoDB(callback) {
+    var db = moClient.db(configs.mongoid);
+    var collection = db.collection('engagements');
+    callback(collection);
+}
+
+module.exports.likesscol = function returnMongoUserGeoDB(callback) {
+    var db = moClient.db(configs.mongoid);
+    var collection = db.collection('likes');
+    callback(collection);
+}
+
+module.exports.notificationscol = function returnMongoUserGeoDB(callback) {
+    var db = moClient.db(configs.mongoid);
+    var collection = db.collection('notifications');
+    callback(collection);
+}
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,14 +2,13 @@ const express                           = require('express');
 const router                            = express.Router();
 const main                              = require('../../../app');
 const bodyParser                        = require('body-parser');
-const path                              = require('path');
 const session                           = require('client-sessions');
-const ok                                = require('async');
-const randomstring                      = require('randomstring');
 const formidable                        = require('formidable');
 const _                                 = require('underscore');
 const mime                              = require('mime');
 const configs                           = require('../../../configs');
+const middleware                        = require('../../../middleware');
+
 
 //  Add projects below
 const dadhiveFunctions                  = require('../../functions/index');
@@ -28,57 +27,223 @@ router.use(session({
     activeDuration: 5 * 60 * 1000,
 }));
 
-router.get('/test', function(req, res) {
+router.post('/test', middleware.checkToken, function(req, res) {
     res.json({
-        "text" : "Test GET request."
-    })
+        "message" : "Test POST request.",
+        "data" : req.body,
+        "page" : {
+            "title": "DadHive",
+            "session": {
+                "isSessionActive": !(req.session !== null || typeof req.session !== 'undefined'),
+                "data": req.session
+            }
+        }
+    });
 });
 
-router.get('/getUsers', function(req, res) {
-    console.log(req.body);
-    dadhiveFunctions.getUsers(req, res);
+// MARK: - Login functionality should be reserved for web platform
+// Web platform not available. 
+
+router.post('/login', function( req, res ) {
+    dadhiveFunctions.signin(req, res);
+});
+
+router.post('/createUser', function(req, res) {
+    dadhiveFunctions.signup(req, res, function(uid, token, refresh) {
+        if (uid === null || token === null || refresh === null) { 
+            var error = {
+                "code": 200,
+                "message": "Something went wrong. Please try again later."
+            }
+            console.log(error);
+            dadhiveFunctions.sendResponse(200, error, null, null, res)
+        } else {
+            req.body.uid = uid
+            req.body.token = token
+            req.body.refreshToken = refresh
+            dadhiveFunctions.createUserMongoDB(req, res);
+        }
+    });
+});
+
+router.get('/signout', function(req, res) {
+    dadhiveFunctions.signout(req, res, function(success){
+        res.redirect('/');
+    });
 });
 
 router.post('/getUser', function(req, res) {
     console.log(req.body);
-    dadhiveFunctions.getUserWithId(req.body.userId, res);
-});
-
-router.post('/createUser', function(req, res) {
-    console.log(req.body);
-    dadhiveFunctions.createUser(req, res);
+    dadhiveFunctions.getUserMongoDB(req, res);
 });
 
 router.post('/createMatch', function(req, res) {
     console.log(req.body);
-    dadhiveFunctions.createMatch(req, res);
+    dadhiveFunctions.createMatchMongoDB(req, res);
 });
 
-router.post('/findMatch', function(req, res) {
+router.post('/getMatches', function(req, res) {
     console.log(req.body);
-    dadhiveFunctions.findMatch(req, res);
+    dadhiveFunctions.getMatchesMongoDB(req, res);
+});
+
+router.post('/deleteMatch', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.deleteMatchMongoDB(req, res);
+});
+
+router.post('/deleteConversation', function(req ,res) {
+    console.log(req.body);
+    dadhiveFunctions.deleteConversationWithParticipantsMongoDB(req, res);
+});
+
+router.post('/addParticipant', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.addParticipantMongoDB(req, res);
+});
+
+router.post('/removeParticipant', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.removeParticipantMongoDB(req, res);
+});
+
+router.post('/createConversation', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.createConversationMongoDB(req, res);
 });
 
 router.post('/findConversations', function(req, res) {
     console.log(req.body);
-    dadhiveFunctions.findConversations(req, res);
+    dadhiveFunctions.getConversationsMongoDB(req, res);
 });
 
-router.post('/findConversation', function(req, res) {
+router.post('/getUsersInConversation', function(req, res) {
     console.log(req.body);
-    dadhiveFunctions.findConversation(req.body.conversationId, res);
+    dadhiveFunctions.getUsersInConversationMongoDB(req, res);
 });
 
-router.post('/getMessages', function(req, res) {
+router.post('/sendMessageCheck', function(req, res) {
     console.log(req.body);
-    dadhiveFunctions.getMessagesInConversation(req.body.conversationId, res);
+    dadhiveFunctions.sendMessageMongoDB(req, res);
 });
 
-router.post('/sendMessage', function(req, res) {
+router.post('/saveLocation', function(req, res) {
     console.log(req.body);
-    dadhiveFunctions.sendMessage(req, res);
+    dadhiveFunctions.saveLocationMongoDB(req, res);
 });
 
+router.post('/editUserProfile', function(req, res) {
+    console.log(req.headers);
+    console.log(req.body);
+    if (req.headers['agent'] === "web") {
+        dadhiveFunctions.editUser(req, res);
+    } else {
+        dadhiveFunctions.editUserMongoDB(req, res);
+    }
+});
+
+router.post('/getNearbyUsers', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.getNearByUsersMongoDB(req, res);
+});
+
+router.post('/retrieveForMap', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.retrieveForMapMongoDB(req, res);
+});
+
+router.post('/reportUser', function(req, res) {
+    dadhiveFunctions.reportUser(req, res);
+});
+
+router.get('/getCategories', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.retrieveAllCategoriesMongoDB(req, res);
+});
+
+router.post('/addCategory', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.addCategoryMongoDB(req, res);
+});
+
+router.post('/addPost', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.addPostMongoDB(req, res);
+});
+
+router.post('/getPosts', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.retrieveAllPostsMongoDB(req, res);
+});
+
+router.post('/getPostsByCategory', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.retrievePostsByCategoryMongoDB(req, res);
+});
+
+router.post('/addEngagement', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.addLikeMongoDB(req, res);
+});
+
+router.post('/getCommentsForPost', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.retrieveCommentsByPostMongoDB(req, res);
+});
+
+router.post('/getActivityForUser', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.retrieveActivityForUserMongoDB(req, res);
+});
+
+router.post('/getUserActivity', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.retrieveOthersActivityForUserMongoDB(req, res);
+});
+
+router.post('/searchForPost', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.retrievePostsBySearchMongoDB(req, res);
+});
+
+router.post('/reportPost', function(req, res) {
+    dadhiveFunctions.reportPost(req, res);
+});
+
+router.post('/blockPost', function(req, res) {
+    dadhiveFunctions.blockPostMongoDB(req, res);
+});
+
+router.post('/blockUser', function(req, res) {
+    dadhiveFunctions.blockUserMongoDB(req, res);
+});
+
+router.post('/deletePost', function(req, res) {
+    dadhiveFunctions.deletePostMongoDB(req, res);
+});
+
+router.post('/getPostsByUser', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.retrievePostsByUserMongoDB(req, res);
+});
+
+router.post('/getBlockedUsers', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.retrieveBlockedUsersMongoDB(req, res);
+});
+
+router.post('/unblockUser', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.unblockUserMongoDB(req, res);
+});
+
+// MARK: - Reserved for Cloud Functions
+router.post('/updateConversation', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.updateConversationMongoDB(req, res);
+});
+
+// MARK: - Not used
 router.post('/uploadPhoto', function(req, res) {
 
     var form = new formidable.IncomingForm();
@@ -121,6 +286,53 @@ router.post('/uploadPhoto', function(req, res) {
 
         }
     });
+});
+
+router.post('/addToMap', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.createMapItem(req, res, function(itemId) {
+        req.body.itemId = itemId
+        dadhiveFunctions.addToMap(req, res);
+    });
+});
+
+router.post('/getMessages', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.getMessagesInConversation(req.body.conversationId, res);
+});
+
+// MARK: - Reserved for ADMIN
+router.get('/getUsers', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.getUsersMongoDB(req, res);
+});
+
+router.get('/deleteAllGeos', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.deleteAllMongoUserGeoElements(req, res);
+});
+
+router.post('/deleteGeo', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.deleteGeo(req, res);
+});
+
+router.post('/deleteGeosBut', function(req, res) {
+    req.body.ids = ["5cf87bf7178341c6ca36ca92", "5cf87f3c178341c6ca37481b"];
+    dadhiveFunctions.deleteGeosBut(req, res);
+})
+
+router.get('/deleteAllActions', function(req, res) {
+    console.log(req.body);
+    dadhiveFunctions.deleteAllMongoActionElements(req, res);
+});
+
+router.post('/deleteAction', function(req, res) {
+    console.log(req.body);
+});
+
+router.post('/deleteUser', function(req, res) {
+    dadhiveFunctions.deleteUser(req, res);
 });
 
 module.exports = router;
